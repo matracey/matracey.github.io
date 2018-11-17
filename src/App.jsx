b67ca5aba7b1d79bb2518886836c7de9f394dc48
Martin Tracey <matracey@outlook.ie>
2018-11-17 15:20:35 +0000

    Added Footer to app component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Header from "./Components/Presentation/Header/Header";
 import Jumbotron from "./Components/Presentation/Jumbotron/Jumbotron";
+import Footer from "./Components/Presentation/Footer/Footer";
 
 import "./App.css";
 
@@ -9,8 +10,11 @@ class App extends Component {
     render() {
         return (
             <div className="App d-flex flex-column">
-                <Header siteTitle={process.env.REACT_APP_NAME} />
-                <Jumbotron header="Welcome!" subtext="This site showcases my work. Please take the time to look around and feel free to reach out to me too!" />
+                <Header className="flex-shrink-0" siteTitle={process.env.REACT_APP_NAME} />
+                <div className="flex-fill d-flex flex-column content">
+                    <Jumbotron header="Welcome!" subtext="This site showcases my work. Please take the time to look around and feel free to reach out to me!" />
+                </div>
+                <Footer className="flex-shrink-0" text="Built in 2018 by Martin Tracey" />
             </div>
         );
     }
