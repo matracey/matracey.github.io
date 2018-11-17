ff8baa3f20823232d5cef5f9cb27bfb019839239
Martin Tracey <matracey@outlook.ie>
2018-11-17 20:43:48 +0000

    Added conditional hr to Project component.

--- src/Components/Presentation/Project/Project.jsx
+++ src/Components/Presentation/Project/Project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 import { PrimaryButton, Image, ImageFit, Link } from "office-ui-fabric-react";
@@ -16,28 +16,32 @@ const Project = props => {
         actionOnClick,
         logoHref,
         logoAlt,
+        showHorizontalRule,
         className
     } = props;
 
     return (
-        <div className={`${className ? `${className} ` : ""}d-flex project-container`}>
-            <div className="flex-fill project-text d-flex flex-column">
-                { titleHref ? (
-                    <Link className="mt-3 ms-font-xxl title" href={titleHref} target="_blank">{title}</Link>
-                ) : (
-                    <div className="mt-3 ms-font-xxl title">{title}</div>
-                )}
-                {subtext && <div className="mt-1 mb-auto ms-font-l subtext">{subtext}</div>}
-                {actionText && (
-                    <div className="mt-3 mh-auto">
-                        <PrimaryButton text={actionText} href={actionHref} target="_blank" onClick={actionOnClick} />
-                    </div>
-                )}
+        <Fragment>
+            <div className={`${className ? `${className} ` : ""}d-flex project-container`}>
+                <div className="flex-fill project-text d-flex flex-column">
+                    { titleHref ? (
+                        <Link className="mt-3 ms-font-xxl title" href={titleHref} target="_blank">{title}</Link>
+                    ) : (
+                        <div className="mt-3 ms-font-xxl title">{title}</div>
+                    )}
+                    {subtext && <div className="mt-1 mb-auto ms-font-l subtext">{subtext}</div>}
+                    {actionText && (
+                        <div className="mt-3 mh-auto">
+                            <PrimaryButton text={actionText} href={actionHref} target="_blank" onClick={actionOnClick} />
+                        </div>
+                    )}
+                </div>
+                <div className="project-logo flex-shrink-0">
+                    <Image src={logoHref || process.env.REACT_APP_LOGO_HREF} alt={logoAlt || title} maximizeFrame imageFit={ImageFit.contain}  />
+                </div>
             </div>
-            <div className="project-logo flex-shrink-0">
-                <Image src={logoHref || process.env.REACT_APP_LOGO_HREF} alt={logoAlt || title} maximizeFrame imageFit={ImageFit.contain}  />
-            </div>
-        </div>
+            {showHorizontalRule && <hr className="w-100" />}
+        </Fragment>
     );
 };
 
@@ -50,6 +54,7 @@ Project.propTypes = {
     actionOnClick: PropTypes.func,
     logoHref: PropTypes.string,
     logoAlt: PropTypes.string,
+    showHorizontalRule: PropTypes.bool,
     className: PropTypes.string
 };
 
