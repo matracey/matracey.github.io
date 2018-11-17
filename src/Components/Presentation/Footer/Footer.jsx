845d05d2b0a86988c4d9d63f83dd28de8012f8de
Martin Tracey <matracey@outlook.ie>
2018-11-17 21:01:47 +0000

    Added githubHref to Footer component.

--- src/Components/Presentation/Footer/Footer.jsx
+++ src/Components/Presentation/Footer/Footer.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Link } from "office-ui-fabric-react"
+
 import "./Footer.css";
 
 const Footer = props => {
-    const { text, className } = props;
+    const { githubHref, rightText, className } = props;
     return (
-        <footer className={`${className ? `${className} ` : ""}d-flex justify-content-end align-items-center ms-bgColor-themeDarker`}>
-            <div className="mr-3 ms-fontColor-white ms-fontWeight-light">{text}</div>
+        <footer
+            className={`${
+                className ? `${className} ` : ""
+            }d-flex ${
+                githubHref ? "justify-content-between" : "justify-content-end"
+            } align-items-center ms-bgColor-themeDarker`}
+        >
+            { githubHref && (
+                <Link
+                    href={githubHref}
+                    className="ml-3 ms-fontColor-white ms-fontColor-neutralQuaternary--hover ms-fontWeight-light d-flex"
+                    target="_blank"
+                >
+                    <i class="fab fa-github ms-fontSize-xl"></i>
+                    <span className="ml-1">matracey</span>
+                </Link>
+            ) }
+            <div className="mr-3 ms-fontColor-white ms-fontWeight-light">{rightText}</div>
         </footer>
     );
 };
 
 Footer.propTypes = {
-    text: PropTypes.string.isRequired,
+    githubHref: PropTypes.string,
+    rightText: PropTypes.string.isRequired,
     className: PropTypes.string,
 }
 
