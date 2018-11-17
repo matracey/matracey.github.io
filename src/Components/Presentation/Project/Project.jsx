310f9a2f8b834cde567ba03c41f17378c0abd39e
Martin Tracey <matracey@outlook.ie>
2018-11-17 17:38:51 +0000

    Implemented Project component.

--- src/Components/Presentation/Project/Project.jsx
+++ src/Components/Presentation/Project/Project.jsx
@@ -1,17 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { PrimaryButton, Image, ImageFit, Link } from "office-ui-fabric-react";
+
 import "./Project.css";
 
+
 const Project = props => {
-    const { className } = props;
+    const {
+        title,
+        titleHref,
+        subtext,
+        actionText,
+        actionHref,
+        actionOnClick,
+        logoHref,
+        logoAlt,
+        className
+    } = props;
+
     return (
-        <h1 className={className}>Project</h1>
+        <div className={`${className ? `${className} ` : ""}d-flex project-container`}>
+            <div className="project-text d-flex flex-column">
+                { titleHref ? (
+                    <Link className="mt-3 ms-font-xxl title" href={titleHref} target="_blank">{title}</Link>
+                ) : (
+                    <div className="mt-3 ms-font-xxl title">{title}</div>
+                )}
+                {subtext && <div className="mt-1 mb-auto ms-font-l subtext">{subtext}</div>}
+                {actionText && (
+                    <div className="mt-3 mh-auto">
+                        <PrimaryButton text={actionText} href={actionHref} target="_blank" onClick={actionOnClick} />
+                    </div>
+                )}
+            </div>
+            {logoHref && (
+                <div className="project-logo">
+                    <Image src={logoHref} alt={logoAlt || title} width="45vw" maximizeFrame imageFit={ImageFit.contain}  />
+                </div>
+            )}
+        </div>
     );
 };
 
 Project.propTypes = {
-    className: PropTypes.string,
-}
+    title: PropTypes.string.isRequired,
+    titleHref: PropTypes.string,
+    subtext: PropTypes.string,
+    actionText: PropTypes.string,
+    actionHref: PropTypes.string,
+    actionOnClick: PropTypes.func,
+    logoHref: PropTypes.string,
+    logoAlt: PropTypes.string,
+    className: PropTypes.string
+};
 
 export default Project;
