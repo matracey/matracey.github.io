4c408378d684ceb1dc10869684d5c46b180d269b
Martin Tracey <matracey@outlook.ie>
2018-11-17 17:59:05 +0000

    Added Projects data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 import Header from "./Components/Presentation/Header/Header";
 import Jumbotron from "./Components/Presentation/Jumbotron/Jumbotron";
 import Footer from "./Components/Presentation/Footer/Footer";
+import Projects from "./Components/Container/Projects/Projects";
+
+import projects from "./projects.json";
 
 import "./App.css";
 
@@ -13,6 +16,7 @@ class App extends Component {
                 <Header className="flex-shrink-0" siteTitle={window.location.hostname} />
                 <div className="flex-fill d-flex flex-column content">
                     <Jumbotron header="Welcome!" subtext="This site showcases my work. Please take the time to look around and feel free to reach out to me!" />
+                    <Projects projects={projects} />
                 </div>
                 <Footer className="flex-shrink-0" text="Built in 2018 by Martin Tracey" />
             </div>
