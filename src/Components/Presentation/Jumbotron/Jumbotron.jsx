a3d531df1e75686d7a850075d947f756a983eba1
Martin Tracey <matracey@outlook.ie>
2018-11-17 14:55:21 +0000

    Added Jumbotron component.

---
+++ src/Components/Presentation/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Jumbotron.css";
+
+const Jumbotron = () => {
+    return (
+        <h1>Jumbotron</h1>
+    )
+};
+
+Jumbotron.propTypes = {}
+
+export default Jumbotron;
