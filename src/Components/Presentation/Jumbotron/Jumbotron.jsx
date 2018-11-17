e2df14e94158ec8066a0ebf89520d2348fc50863
Martin Tracey <matracey@outlook.ie>
2018-11-17 15:07:56 +0000

    Implemented Jumbotron component.

--- src/Components/Presentation/Jumbotron/Jumbotron.jsx
+++ src/Components/Presentation/Jumbotron/Jumbotron.jsx
@@ -3,12 +3,21 @@ import PropTypes from "prop-types";
 
 import "./Jumbotron.css";
 
-const Jumbotron = () => {
+const Jumbotron = props => {
+    const { header, subtext } = props;
     return (
-        <h1>Jumbotron</h1>
-    )
+        <div className="ms-bgColor-neutralLighter jumbotron-container">
+            <div className="ms-font-su jumbotron-header">{header}</div>
+            <div className="ms-fontSize-xl ms-fontWeight-light jumbotron-subtext">
+                {subtext}
+            </div>
+        </div>
+    );
 };
 
-Jumbotron.propTypes = {}
+Jumbotron.propTypes = {
+    header: PropTypes.string.isRequired,
+    subtext: PropTypes.string
+};
 
 export default Jumbotron;
