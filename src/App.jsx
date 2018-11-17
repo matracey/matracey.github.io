da5755cf78e70dfa2891879500021fafae6d4ce5
Martin Tracey <matracey@outlook.ie>
2018-11-17 14:36:49 +0000

    Added Header to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+import Header from "./Components/Presentation/Header/Header";
+
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App d-flex flex-column">
-                <h1>Hello!</h1>
+                <Header />
             </div>
         );
     }
